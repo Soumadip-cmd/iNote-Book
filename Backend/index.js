dddc29f890a4830b98de751c26a41ea3e98cc7f0
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-15 17:41:34 +0530

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const port = 8000;
 const app = express();
  
 app.use(cors({
-  origin: 'https://i-note-book-frontend-theta.vercel.app/' // Adjusted to match your frontend port
+  origin: 'https://i-note-book-frontend-theta.vercel.app' // Adjusted to match your frontend port
 }));
 
 app.get('/',(req,res)=>{
