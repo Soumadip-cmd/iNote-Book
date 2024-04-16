a5d8c8f8f74c2184c690950eb63d90ac9ad08077
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-16 12:11:03 +0530

    using cors

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,11 @@ mongodbtoConnect();
 const port = 8000;
 const app = express();
  
-app.use(cors())
+app.use(cors({
+  origin:'https://i-note-book-frontend-theta.vercel.app'
+}
+  
+))
 
 app.get("/",(req,res)=>{
   res.send("hello boy")
